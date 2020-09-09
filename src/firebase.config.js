4c5b83f9bfd224d34f58f7a14b9bd941fdc46312
MiMNets <mdmonirulislammonir@gmail.com>
2020-09-09 07:25:24 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAUBcS8Pcxug18CtZjqwYxNydhOUjaV0RA",
+    authDomain: "ema-john-simple-projects.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-projects.firebaseio.com",
+    projectId: "ema-john-simple-projects",
+    storageBucket: "ema-john-simple-projects.appspot.com",
+    messagingSenderId: "214787728532",
+    appId: "1:214787728532:web:bfe052f16eb2d74b894081"
+  };
+
+  export default firebaseConfig;
